e6da13858561a2b2c4f9cafc5deb9b20604f31e1
daniel ung <daniel.ung@sjsu.edu>
2025-07-05 20:10:12 -0400

    model

--- tiny-backend/tiny-ml/model.py
+++ tiny-backend/tiny-ml/model.py
@@ -12,6 +12,7 @@ class ChessNet:
     """
     def __init__(self, num_moves=4672):
         self.resnet_body = ResNet(18) #resnet-18
+        #(1, 12, 8, 8) → (1, 64, 8, 8)
         self.resnet_body.conv1 = Conv2d(12, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.resnet_body.fc = None
         resnet_out_features = 512 * self.resnet_body.block.expansion
